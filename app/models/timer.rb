6044cc5c69113ac77c4d1b417c8f3763da53306c
David Haley <djdavidhaley@gmail.com>
2016-07-14 15:27:37 -0600

    further testing suckerpunch

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -1,14 +1,8 @@
-class Timer
-  include SuckerPunch::Job
-  # workers 1
+# class Timer
+#   include SuckerPunch::Job
+#   # workers 1
 
-  def perform(repo_url)
-    resp = HTTParty.get(repo_url)
-    # puts resp
-    resp = JSON.parse resp.body
-    interval = 5
-    respond_message("Time interval = #{interval}")
-  end
-end
-
-# respond_message("There are #{resp['open_issues_count']} open issues on #{}. Time interval = #{interval}")
+#   def perform(message)
+#     message
+#   end
+# end
