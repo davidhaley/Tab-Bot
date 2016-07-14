d887e5900c29b748dfe40ed2a2b85ce0edbd1e6f
David Haley <davidhaley@protonmail.com>
2016-07-14 11:55:31 -0600

    further testing

--- app/actions.rb
+++ app/actions.rb
@@ -9,14 +9,14 @@ post '/gateway' do
   action, repo = message.split('_').map {|c| c.strip.downcase }
   puts action
   puts repo.inspect
-  # repo_url = "https://api.github.com/repos/#{repo}"
+  repo_url = "https://api.github.com/repos/#{repo}"
 
-  # case action
-  #   when 'issues'
-  #     resp = HTTParty.get(repo_url)
-  #     resp = JSON.parse resp.body
-  #     respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
-  # end
+  case action
+    when 'issues'
+      resp = HTTParty.get(repo_url)
+      resp = JSON.parse resp.body
+      respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
+  end
 end
 
 def respond_message message
