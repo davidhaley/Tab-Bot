a41cd8b9627fc43dcbcd7bca8be9f83eb5f39bf3
David Haley <davidhaley@protonmail.com>
2016-07-14 15:46:29 -0600

    further testing suckerpunch

--- app/actions.rb
+++ app/actions.rb
@@ -10,6 +10,7 @@ post '/gateway' do
   message = params[:text].gsub(params[:trigger_word], '').strip
   # puts message
   action, repo = message.split('_').map {|c| c.strip.downcase }
+
   # puts action
   # puts repo.inspect
   repo_url = "https://api.github.com/repos/#{repo}"
