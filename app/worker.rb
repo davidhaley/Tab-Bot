9ae2a123d9f2b922bc1b57455d734da031a72c9d
David Haley <davidhaley@protonmail.com>
2016-07-15 16:58:20 -0600

    further testing interval variable

--- app/worker.rb
+++ app/worker.rb
@@ -1,21 +1,3 @@
-# require 'rubygems'
-# require 'bundler/setup'
-# APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
-# require 'active_support/all'
-# require 'sinatra'
-# require 'sinatra/activerecord'
-# require 'sinatra/contrib/all'
-# require 'active_record'
-# require_relative '../config/database'
-# require 'json'
-# require_relative 'models/message'
-# require 'sucker_punch'
-
-# Timer.create(interval: 20)
-# Log.create(notified_at: DateTime.now)
-
-# check for the next time to send message
-
 class Worker
 
   def initialize(team, interval)
@@ -44,8 +26,3 @@ class Worker
     @running = false
   end
 end
-
-# Worker.new
-
-# loop over every timer and find out when each one ran and whether you should do another one.
-
