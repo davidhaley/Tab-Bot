890243f53923afcec1d326c2bf798d6ef1468ece
David Haley <djdavidhaley@gmail.com>
2016-07-15 17:05:10 -0600

    further testing interval variable

--- app/worker.rb
+++ app/worker.rb
@@ -8,12 +8,11 @@ class Worker
 
   def start
     while @team.timer.running
-      puts DateTime.now
       if DateTime.now < Log.last.notified_at + @team.timer.interval.seconds
+        puts "team timer interval: #{@team.timer.interval.seconds}"
         sleep(2)
         puts "sleeping for 2 seconds"
       else
-        puts "performing message at #{Time.now}"
         Message.perform_in(1)
         Timer.create(interval: @interval)
         Log.create(notified_at: DateTime.now)
