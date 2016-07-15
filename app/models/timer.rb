2fa990bdc68c75d8236dc48e07553a38030a6a20
David Haley <davidhaley@protonmail.com>
2016-07-14 19:57:14 -0600

    removed loop

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -4,14 +4,14 @@ class Timer
 
   SuckerPunch.shutdown_timeout = 30
 
-  def perform(working=true)
+  def perform
     HTTParty.post("https://hooks.slack.com/services/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", 
     :body => { "channel": "#general", "username": "webhookbot", "text": "This is posted to #general and comes from a bot named webhookbot.", "icon_emoji": ":ghost:"}.to_json,
     :headers => { 'Content-Type' => 'application/json' } )
-    if working
-      Timer.perform_in(1, false)
-    else
-      Timer.perform_in(10, true)
-    end
+    # if working
+    #   Timer.perform_in(1, false)
+    # else
+    #   Timer.perform_in(10, true)
+    # end
   end
 end
