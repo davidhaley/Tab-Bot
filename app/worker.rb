21072602bb56eb35a3dccb145cc0399bcb2c0189
David Haley <davidhaley@protonmail.com>
2016-07-15 17:18:47 -0600

    further testing interval variable

--- app/worker.rb
+++ app/worker.rb
@@ -8,15 +8,15 @@ class Worker
 
   def start
     while @team.timer.running
-      # if DateTime.now < Log.last.notified_at + @team.timer.interval.seconds
-      #   puts "team timer interval: #{@team.timer.interval.seconds}"
-      sleep(2)
-      #   puts "sleeping for 2 seconds"
-      # else
-      #   Message.perform_in(1)
-      #   Timer.create(interval: @interval)
-      #   Log.create(notified_at: DateTime.now)
-      # end
+      if DateTime.now < Log.last.notified_at + @team.timer.interval.seconds
+        puts "team timer interval: #{@team.timer.interval.seconds}"
+        puts "sleeping for 2 seconds"        
+        sleep(2)
+      else
+        Message.perform_in(1)
+        Timer.create(interval: @interval)
+        Log.create(notified_at: DateTime.now)
+      end
       @team.timer.reload
     end
   end
