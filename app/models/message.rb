650e7577fccecd9e7cc568a52d1cbf1beb739ea6
David Haley <djdavidhaley@gmail.com>
2016-07-15 19:17:05 -0600

    further testing interval variable

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,11 +6,11 @@ class Message
 
   def perform(greeting=nil, team=nil, interval=nil)
     if greeting == "start"
-      message = "...TaB-bot ONLINE! Welcome #{team}! My break timer is set to: #{interval} seconds!"
+      message = "...TaB-bot ONLINE!\n\nWelcome #{team}! Get to work.. You'll be hearing from me in #{interval} <seconds!></seconds!>"
     elsif greeting == "stop"
       message = "...TaB-bot OFFLINE!"
     else
-      message = "You've been working for a while! Make sure you take a break!\nMessage Time: #{DateTime.now}"
+      message = "You've been working for a while! Make sure you take a break!\n\nMessage Time: #{DateTime.now}"
     end
     puts "performing message at #{Time.now}"
     HTTParty.post("https://hooks.slack.com/services/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", 
