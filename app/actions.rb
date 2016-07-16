6feb3650890082ea5e38228de22e14b018dbeb76
David Haley <davidhaley@protonmail.com>
2016-07-16 17:16:46 -0600

    removed team name variable

--- app/actions.rb
+++ app/actions.rb
@@ -17,6 +17,7 @@ post '/gateway' do
   team_name = "team"
 
   interval ||= 1
+  interval.count > 4 ? Message.perform_in(1, "interval_count") : interval
   interval = interval.to_i * 60
 
   team = Team.find_or_create_by(name: team_name)
