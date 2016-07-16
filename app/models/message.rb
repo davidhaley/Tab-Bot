08aaeeabc10efe3f22329386c282036b520733cd
David Haley <davidhaley@protonmail.com>
2016-07-16 17:08:23 -0600

    removed team name variable

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,7 +10,7 @@ class Message
     elsif greeting == "stop"
       message = "...TaB-bot OFFLINE!"
     elsif greeting == "help"
-      message = "Welcome to help!\n\nStart tab-bot by issuing a command:\n\n_*tab-bot:*  *[*start|stop|help*]* *[*break-interval-in-minutes*]*_\n\nexamples: _*tab-bot: start 60*_\n\nstarts _'tab-bot'_ with a break-timer of _'60'_ minutes\n\nexample with default settings: _*tab-bot: start*_\n\nstarts _'tab-bot'_ with a break-timer of _'2'_ minutes (default)"
+      message = "Welcome to help!\n\nStart tab-bot by issuing a command:\n\n_*tab-bot:*  *[*start|stop|help*]* *[*break-interval-in-minutes*]*_\n\nexamples: _*tab-bot: start 60*_\n\nstarts _'tab-bot'_ with a break-timer of _'60'_ minutes\n\nbreak-timer default is: _'2' minutes_"
     else
       message = "Hey team! You've been working for a while! Have a break!\n\n#{(Time.now + 18*60*60).strftime("%Y-%m-%d %H:%M:%S")}"
     end
