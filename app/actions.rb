62f4a0f0d40db0b26da1d225f928eaec273c9e54
David Haley <djdavidhaley@gmail.com>
2016-07-16 15:18:49 -0600

    trying to fix timer that won't stop

--- app/actions.rb
+++ app/actions.rb
@@ -43,8 +43,8 @@ post '/gateway' do
       @worker.start
     when 'stop'
       Message.perform_in(1, "stop")
-      team.timer.update(running: false)
-      team.timer.save
+      team.timer.update!(running: false)
+      team.timer.save!
     else
       Message.perform_in(1, "help")
   end
